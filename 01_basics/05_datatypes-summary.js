8615aea9e116097170fbb82b046b01ddc3a28363
Tanishq-Backend-Dev <154458664+Tanishq-Backend-Dev@users.noreply.github.com>
2024-12-16 20:36:00 +0000

    commit 'datatypes-summary'

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,23 @@
+// Primitive (Passed by value)
+
+// 7 Types -> String, Number, BigInt, Boolean, Undefined, Null, Symbol
+
+const score = 100;
+const scoreValue = 122.34;
+const isLoggedIn = true;
+const outsideTemp = null;
+let userEmail;
+
+// Non- Primitive type (Passed by reference)
+
+// Object, Array, Function
+
+const heros = ['Supermn', 'Batman', 'Spiderman'];
+let myObj = {
+    name: 'Tanishq',
+    age: 23
+}
+
+const muFunction = () => {
+    console.log(`Hello World`);
+}
